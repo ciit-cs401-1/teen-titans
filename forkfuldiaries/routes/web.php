42efb655d78345a79b6bda6172ccd90f3c449823
Vhiane Gulapa <vhiane.gulapa@ciit.edu.ph>
2025-07-31 07:03:24 +0800

    wip of Admin role

---
+++ forkfuldiaries/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/**
+ * TESTING ROUTES
+ */
+Route::view('/example-page','example-page');
+Route::view('/example-auth','example-auth');
+
+
+/**
+ * ADMIN ROUTES
+ */
+
+Route::prefix('admin')->name('admin.')->group(function(){
+    Route::middleware([])->group(function(){
+        Route::controller(AuthController::class)->group(function(){
+            Route::get('/login','loginForm')->name('login');
+            Route::post('/login','loginHandler')->name('login_handler');
+            Route::get('/forgot-password','forgotForm')->name('forgot');
+            Route::post('/send-password-reset-link','sendPasswordResetLink')->name('send_password_reset_link');
+            Route::get('/password/reset/{token}','resetForm')->name('reset_password_form');
+        });
+    });
+
+    Route::middleware([])->group(function(){
+        Route::controller(AdminController::class)->group(function(){
+            Route::get('/dashboard','adminDashboard')->name('dashboard');
+            Route::post('/logout', 'logoutHandler')->name("logout");
+        });
+    });
+});
