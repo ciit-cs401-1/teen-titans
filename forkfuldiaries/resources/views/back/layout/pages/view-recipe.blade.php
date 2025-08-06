5ab1e3837e1ebf8fb65dbb37cc9995f82ab725c1
Tsu <kathyengallaa@gmail.com>
2025-08-06 19:28:40 +0800

    line break !!

--- forkfuldiaries/resources/views/back/layout/pages/view-recipe.blade.php
+++ forkfuldiaries/resources/views/back/layout/pages/view-recipe.blade.php
@@ -12,7 +12,7 @@
     <div class="mt-6">
         <h2 class="text-xl font-semibold text-[#4d2100] mb-2">Recipe Content</h2>
         <div class="bg-gray-100 p-4 rounded text-gray-900 whitespace-pre-wrap">
-            {{ $recipe->recipes_file }}
+            <p>{!! nl2br(e($recipe->recipes_file)) !!}</p>
         </div>
     </div>
 
